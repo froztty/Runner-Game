96a6f4428da61449354778a59cb1126f9e4daa52
froztty <91167567+froztty@users.noreply.github.com>
2024-02-07 21:56:43 -0800

    Complete game without states
    player gravity function and death

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300)) # midbottom is the position of the rectangle so it is on that point. so try midtop or topright
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
@@ -24,8 +25,13 @@ while True:
             pygame.quit()
             # could use a break to get out of the game but we should use sys
             exit()
-        # if event.type == pygame.MOUSEMOTION: # there are other thigns to check the event like MOUSEBUTTOWNDOWN or UP
-        #     if player_rect.collidepoint(event.pos): print('collision')
+        if event.type == pygame.MOUSEBUTTONDOWN: # there are other thigns to check the event like MOUSEBUTTOWNMOTION or UP
+            if player_rect.collidepoint(event.pos)  and player_rect.bottom >= 300: # checks to make sure player is standing on ground
+                player_gravity = -20
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20 # adds jump motion
+        
 
     screen.blit(sky_surf, (0, 0)) # coords (x, y)
     screen.blit(ground_surf, (0, 300))
@@ -38,14 +44,27 @@ while True:
     if snail_rect.right <= 0 : snail_rect.left = 800 # this checks if the right side of the snail is gone then the left side will appear on the other side of the screen
     screen.blit(snail_surf, snail_rect)
     #player_rect.left += 1 # moves player
+    
+    #Player
+    player_gravity += 1
+    player_rect.y += player_gravity # there is an increase in speed. adds 2 to 301 then adds 3 to 305 etc. exponential 
+    if player_rect.bottom > 300: player_rect.bottom = 300 # prevent the player from going past the ground
     screen.blit(player_surf, player_rect)
     
-
-    # player_rect.colliderect(snail_rect) returns 1 or 0
+    # collision
+    if snail_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
     
-    mouse_pos = pygame.mouse.get_pos()
+    #mouse_pos = pygame.mouse.get_pos()
+    #we can get mouse input or keyboard input via through pygame or in the event loop
     #if player_rect.collidepoint(mouse_pos):
         #print(pygame.mouse.get_pressed())
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print('jump')
+    # player_rect.colliderect(snail_rect) returns 1 or 0
     
     pygame.display.update()
     clock.tick(60)
